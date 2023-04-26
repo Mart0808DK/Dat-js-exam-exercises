ad7f162da6226b97ea3a87eace906035620f3ba0
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-26 16:52:52 +0200

    enviroment 2 not fi

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,31 @@
 "use strict";
 
+window.addEventListener("load", main) 
+
+function main() {
+    console.log("det kører ")
+
+    document.querySelector("#create-form").addEventListener("submit", btnSubmit)
+}
+
+const animalN = document.querySelector("#animal-name")
+const animalT = document.querySelector("#animal-type")
+const animalA = document.querySelector("#animal-age")
+
+function btnSubmit(event) {
+    event.preventDefault();
+    console.log("Hello");
+    showAnimal();
+    
+}
+
+
+function showAnimal() {
+const myhtml = /*html*/ `
+<td>${animalN.vaule}</td>
+<td>${animalT.value}</td>
+<td>${animalA.value}</td>
+`;
+
+document.querySelector("#list-container").insertAdjacentHTML("beforeend", myhtml);;
+}
