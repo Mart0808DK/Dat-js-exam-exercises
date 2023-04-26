89afc8ce4f80845320c484099c607945539ab97f
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-26 12:27:06 +0200

    Enviromment 01

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,28 @@
 "use strict";
 
+window.addEventListener("load", main)
+
+async function main() {
+    const users = await getJson()
+
+    users.forEach(showUsers)
+
+    console.log(users)
+}
+
+async function getJson() {
+    const response = await fetch("users.json")
+
+    const data = await response.json()
+    return data
+}
+
+function showUsers(users) {
+   console.log(users)
+
+   const myHtml = /*html*/ `
+   <article>
+   <li>${users.name} - active ${users.active}</li>
+   </article>
+   `
+} 
