ee087baddd846c12c4b71de23fd1a1ba178796e6
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-26 16:17:08 +0200

    Envirmoment 1 done

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -2,27 +2,48 @@
 
 window.addEventListener("load", main)
 
+// del 1 
 async function main() {
-    const users = await getJson()
+    const users = await getUsers() 
 
-    users.forEach(showUsers)
+    const admin = getAdmin(users)
+
+    admin.forEach(showUsers);
+
+    // users.forEach(showUsers());
+
 
     console.log(users)
 }
 
-async function getJson() {
-    const response = await fetch("users.json")
+async function getUsers() {
+    const response = await fetch("users.json");
 
-    const data = await response.json()
-    return data
+    const data = await response.json();
+
+    return data;
 }
 
+// del 2
 function showUsers(users) {
-   console.log(users)
+    const myHtml = /*html*/ `
+    <article>
+    <li>${users.name} active : ${users.active}</li>
+    </article>
+    `;
 
-   const myHtml = /*html*/ `
-   <article>
-   <li>${users.name} - active ${users.active}</li>
-   </article>
-   `
-} 
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", myHtml)
+
+}
+
+// del 3 ¨
+
+function getAdmin(users) {
+    const response = users.filter(filterAdmin)
+    function filterAdmin(user) {
+        return user.role === "admin";
+    }
+
+    return response;
+}
+
