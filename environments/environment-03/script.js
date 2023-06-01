1e9160086527a99a50cc20373d363d823e824182
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-06-01 12:59:13 +0200

    part 1 and part 2 done part 3 missing

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,1 +1,63 @@
 "use strict";
+
+const products = [
+    {
+        name: "Cocio",
+        price: 15,
+        inStock: true,
+    },
+    {
+        name: "Mælk",
+        price: 10,
+        inStock: true,
+    },
+    {
+        name: "Pringels",
+        price: 20,
+        inStock: false,
+    },
+];
+
+window.addEventListener("load", main);
+
+function main() {
+    console.log("alt kører");
+    document.querySelector("#create-form").addEventListener("submit", createNewProduct);
+
+    displayProducts(products);
+    console.log(products);
+}
+
+function displayProducts(products) {
+    document.querySelector("#list-container").innerHTML = "";
+    for (const product of products) {
+        showProducts(product);
+    }
+}
+
+function showProducts(product) {
+    if (product.inStock) {
+        const myHtml = `
+    <ul>
+    <li>${product.name}, ${product.price}, ${product.inStock}</li>
+    </ul>
+    `;
+
+        document.querySelector("#list-container").insertAdjacentHTML("beforeend", myHtml);
+    }
+}
+
+function createNewProduct(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.valeu;
+    const price = Number(form.price.valeu);
+    const inStock = Boolean(form.inStock.valeu);
+
+    const newProduct = {name, price, inStock};
+
+    products.push(newProduct);
+    console.log(products);
+    console.log(newProduct);    
+}
