948efe52f67984e8642e16f9918e105fd14f3759
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-06-01 12:35:38 +0200

    part 1 and part 2 almost done

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,48 @@
 "use strict";
 
+const products = [
+    {
+        name: "Cocio",
+        price: 15,
+        inStock: true,
+    },
+    {
+        name: "Mælk",
+        price: 10,
+        inStock: true,
+    },
+    {
+        name: "Pringels",
+        price: 20,
+        inStock: false,
+    },
+];
+
+
+window.addEventListener("load", main) 
+
+function main() {
+    console.log("alt kører");
+
+    displayProducts(products)
+}
+
+function sortListByISstock(product1, product2) {
+    return sortListByISstock(())
+}
+
+function displayProducts(products) {
+    for (const product of products) {
+        showProducts(product)
+    }
+}
+
+function showProducts(product) {
+    const myHtml = `
+    <tr>
+    <td>${product.name}, ${product.price}, ${product.inStock}</td>
+    </tr>
+    `
+
+    document.querySelector("tbody").insertAdjacentHTML("beforeend", myHtml)
+}
