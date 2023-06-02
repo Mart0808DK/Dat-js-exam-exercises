0acda6ae4272b46682c4844f7c5c6ed1bca85d4f
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-06-02 12:05:47 +0200

    part 3 missing

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+window.addEventListener("load", main)
+
+async function main() {
+    const playlist = await getPlaylist()
+
+    playlist.forEach(showPlaylist)
+    console.log(playlist);
+}
+
+async function getPlaylist() {
+    const response = await fetch("playlist.json")
+
+    const data = await response.json();
+
+    return data
+}
+
+function showPlaylist(playlists) {
+    const myHtml = `
+    <li>${playlists.artist}, ${playlists.title}, ${playlists.duration}</li>
+    <button class="removeBtn">Remove</button>
+    `;
+
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", myHtml);
+    document.querySelector(".removeBtn").addEventListener("click", () => removeBtn(playlists))
+}
+
+function removeBtn(playlists) {
+    console.log("Hej");
+}
